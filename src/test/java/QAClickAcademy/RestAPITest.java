f629194e05b04a8cee7e3f8ff18a2182445678a0
gurpreet <gksambee@gmail.com>
2019-12-07 15:10:03 -0500

    This is commit from 2nd User

--- src/test/java/QAClickAcademy/RestAPITest.java
+++ src/test/java/QAClickAcademy/RestAPITest.java
@@ -6,7 +6,9 @@ import org.testng.annotations.Test;
 public class RestAPITest {
 	@Test
 	public void PostJira() {
-		System.out.println("PostJira");
+		System.out.println("PostJira1");
+		System.out.println("PostJira2");
+		System.out.println("PostJira3");
 	}
 	@Test
 	public void DeleteTwitter() {
