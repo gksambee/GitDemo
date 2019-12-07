bac7476cf120cc0120edccff6bb7529d36f1cff7
gurpreet <gksambee@gmail.com>
2019-12-07 14:34:51 -0500

    First Commit

---
+++ src/test/java/QAClickAcademy/AppiumTest.java
@@ -0,0 +1,16 @@
+package QAClickAcademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAPPAndriod() {
+		System.out.println("NativeAPPAndriod");
+	}
+
+	@Test
+	public void IOSApp() {
+		System.out.println("IOSApp");
+	}
+}
