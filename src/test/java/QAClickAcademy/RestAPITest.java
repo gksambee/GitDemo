ff4bf8aef9ecc6f72b724df1271d8f797bfd0b57
gurpreet <gksambee@gmail.com>
2019-12-07 15:25:14 -0500

    Third Commit by Ammerican Guy

--- src/test/java/QAClickAcademy/RestAPITest.java
+++ src/test/java/QAClickAcademy/RestAPITest.java
@@ -9,6 +9,10 @@ public class RestAPITest {
 		System.out.println("PostJira1");
 		System.out.println("PostJira2");
 		System.out.println("PostJira3");
+		System.out.println("PostJira4");
+		System.out.println("PostJira5");
+		System.out.println("PostJira6");
+		System.out.println("PostJira7");
 	}
 	@Test
 	public void DeleteTwitter() {
